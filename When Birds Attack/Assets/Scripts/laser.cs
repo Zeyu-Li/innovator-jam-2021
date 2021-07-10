c72d30e1832e83a68543f3a8f7042e1372d8dd04
Zeyu-Li <zeyu.li23@gmail.com>
2021-07-10 11:57:10 -0600

    added sounds

---
+++ When Birds Attack/Assets/Scripts/laser.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laser : MonoBehaviour
+{
+    public float speed = 5f;
+    // audio
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+    Rigidbody rb;
+    Vector3 tmp;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.up * -speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider collision) {
+        if (collision.gameObject.CompareTag("Ground")) {   
+            // destroy item + sound
+            audioSource.PlayOneShot(audioClip, 1f);
+            Destroy(gameObject);
+        } else if (collision.gameObject.CompareTag("Player")) {
+            // decrease player health
+        }
+    }
+}
