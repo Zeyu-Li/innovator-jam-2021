b80e8937e4c4cd0cc07d1ef045b35cc6551b80b1
Zeyu-Li <zeyu.li23@gmail.com>
2021-07-10 07:03:58 -0600

    added load in and pause menu

---
+++ When Birds Attack/Assets/Scripts/loadIn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadIn : MonoBehaviour
+{
+    public GameObject fog;
+    public float startDistance = 7.43f;
+    public float endDistance = 0f;
+    // Movement speed in units per second.
+    public float speed = 1.0f;
+    
+    // Time when the movement started.
+    private float startTime;
+    void Start()
+    {
+        fog.transform.position = new Vector3(0, startDistance, 0);
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distCovered = (Time.time - startTime) * speed;
+        float fractionOfJourney = distCovered / (startDistance-endDistance);
+        fog.transform.position = Vector3.Lerp(new Vector3(0, startDistance, 0), new Vector3(0, endDistance, 0), fractionOfJourney);
+        
+    }
+}
---
+++ When Birds Attack/Assets/Scripts/quit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class quit : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)) // change escape to any other character if you like 
+        {
+            Application.Quit();
+        } else if (Input.GetKeyDown(KeyCode.R)) // change escape to any other character if you like 
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    
+    }
+}
