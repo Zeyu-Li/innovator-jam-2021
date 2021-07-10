e15cc3f91dfaff067767687137e01c762eedca16
Zeyu-Li <zeyu.li23@gmail.com>
2021-07-10 15:53:06 -0600

    fixed die after win bug

--- When Birds Attack/Assets/Scripts/win.cs
+++ When Birds Attack/Assets/Scripts/win.cs
@@ -6,11 +6,14 @@ public class win : MonoBehaviour
 {
     public GameObject winEvent;
     public GameObject shooter;
+    public GameObject healthManger;
 
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider collision) {
         if (collision.gameObject.CompareTag("Player")) {
             shooter.GetComponent<shooter>().pause = true;
+            // set to negative one so player cannot die
+            healthManger.GetComponent<healthManager>().health = -1;
             winEvent.SetActive(true);
         }
     }
