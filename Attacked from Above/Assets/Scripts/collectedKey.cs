10fd3664e5e05d7febf383a2c4d276423ecea89f
Zeyu-Li <zeyu.li23@gmail.com>
2021-07-11 15:44:44 -0600

    added bonus key

---
+++ Attacked from Above/Assets/Scripts/collectedKey.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectedKey : MonoBehaviour
+{
+    
+    // audio
+    AudioSource audioSource;
+    public AudioClip collectClip;
+
+    void Start() {
+        audioSource = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            // set pref
+            PlayerPrefs.SetInt("coin", 1);
+            audioSource.PlayOneShot(collectClip, .5f);
+
+            // destroy and play sound
+            Destroy(gameObject);
+
+        }
+    }
+}
