95b44b0df494530e3902ff0ed4bb99fb5917f30c
Zeyu-Li <zeyu.li23@gmail.com>
2021-07-11 16:07:52 -0600

    almost finished bonus level

---
+++ Attacked from Above/Assets/Scripts/quit2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class quit2 : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)) // change escape to any other character if you like 
+        {
+            Application.Quit();
+        } else if (Input.GetKeyDown(KeyCode.R)) // change escape to any other character if you like 
+        {
+            Time.timeScale = 1f;
+            // destroy music
+            try {
+                Destroy(GameObject.Find("Music"));
+            } catch {
+                ;
+            }
+            SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ Attacked from Above/Assets/Scripts/realWin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class realWin : MonoBehaviour
+{
+    
+    // audio
+    AudioSource audioSource;
+    public GameObject winScreen;
+    private void OnTriggerEnter(Collider collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            // fade to white then show win screen
+
+        }
+    }
+}
